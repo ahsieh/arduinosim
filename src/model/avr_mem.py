e2629e155ec8239e7cad19ca358f58ea3503db99
Alex Hsieh <alex@atlaswristband.com>
2014-04-24 15:16:54 -0500

    Added a register obj, which inherits from avr_mem. Has added capabilities of bit manipulation.

--- src/model/avr_mem.py
+++ src/model/avr_mem.py
@@ -22,22 +22,22 @@ class AVRMemoryByte:
 
     # String Representation
     def __str__(self):
-        return "reg@0x%(addr)02X: 0x%(val)02X" % \
+        return "mem@0x%(addr)02X: 0x%(val)02X" % \
                 { "addr" : self.addr , "val" : self.read() }
 
     # Object Comparison
     # TODO
 
     # Instance methods
-    # Read register contents
+    # Read memister contents
     def read(self):
         return self.contents & self.read_bitmask
 
-    # Modify register contents
+    # Modify memister contents
     def write(self, contents):
         self.contents = contents & self.write_bitmask
 
-    # Increment register contents
+    # Increment memister contents
     # Return 1 if overflow occurs, 0 otherwise
     def incr(self):
         retval = 0
@@ -47,7 +47,7 @@ class AVRMemoryByte:
         self.contents = result & self.write_bitmask
         return retval
 
-    # Decrement register contents
+    # Decrement memister contents
     # Return 1 if overflow occurs, 0 otherwise
     def decr(self):
         retval = 0
@@ -57,7 +57,7 @@ class AVRMemoryByte:
         self.contents = result & self.write_bitmask
         return retval
 
-    # Add to register contents
+    # Add to memister contents
     # Return 1 if overflow occurs, 0 otherwise
     def add(self, value):
         retval = 0
@@ -67,7 +67,7 @@ class AVRMemoryByte:
         self.contents = result & self.write_bitmask
         return retval
 
-    # Subtract from register contents
+    # Subtract from memister contents
     # Return 1 if overflow occurs, 0 otherwise
     def sub(self, value):
         retval = 0
@@ -77,45 +77,45 @@ class AVRMemoryByte:
         self.contents = result & self.write_bitmask
         return retval
 
-    # Set bits in register
-    def set_bits(self, bm):
-        self.contents = (self.contents | bm) & self.write_bitmask
-
-    # Clear bits in register
-    def clear_bits(self, bm):
-        self.contents = (self.contents & ~bm) & self.write_bitmask
-
-    # Nibble swap
-    # TODO?
-
 # Main function -------------------------------------------------------------
 ## Main (for testing purposes)
 if __name__=="__main__":
-    reg = AVRMemoryByte(0x00)
-    print reg
-    reg.write(0xAA)
-    print reg
-    reg.write(0x3F)
-    print reg
-    reg.set_bits(0xC0)
-    print reg
-    reg.clear_bits(0xC0)
-    print reg
-    reg.write(0xFE)
-    print reg
-    print reg.incr()
-    print reg
-    print reg.incr()
-    print reg
-    print reg.decr()
-    print reg
-    print reg.decr()
-    print reg
+    print "Creating new memory byte object"
+    mem = AVRMemoryByte(0x00)
+    print mem
+
+    print "Writing 0xAA to memory byte"
+    mem.write(0xAA)
+    print mem
+
+    print "Writing 0x3F to memory byte"
+    mem.write(0x3F)
+    print mem
+
+    print "Writing 0xFE to memory byte"
+    mem.write(0xFE)
+    print mem
+
+    print "Incrementing memory byte (should be 0xFF, no overflow/carry)"
+    ovf = mem.incr()
+    print str(mem) + " overflow: " + str(ovf)
+
+    print "Incrementing memory byte (should be 0x00 with overflow/carry)"
+    ovf = mem.incr()
+    print str(mem) + " overflow: " + str(ovf)
+
+    print "Decrementing memory byte (should be 0xFF with overflow/carry)"
+    ovf = mem.decr()
+    print str(mem) + " overflow: " + str(ovf)
+
+    print "Decrementing memory byte (should be 0xFE, no overflow/carry)"
+    ovf = mem.decr()
+    print str(mem) + " overflow: " + str(ovf)
 
     print "............................"
-    regs = []
+    mems = []
     for x in xrange(32):
-        regs.append(AVRMemoryByte(x))
-    for x in xrange(len(regs)):
-        print regs[x]
+        mems.append(AVRMemoryByte(x))
+    for x in xrange(len(mems)):
+        print mems[x]
 
---
+++ src/model/avr_reg.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+# Imports -------------------------------------------------------------------
+from avr_mem import AVRMemoryByte
+
+# Classes -------------------------------------------------------------------
+## Generic Memory Byte Object 
+class AVRRegister(AVRMemoryByte):
+    # Instance variables
+
+    # Override methods
+    # Initialization
+    def __init__(self, addr, contents=0x00):
+        AVRMemoryByte.__init__(self, addr, contents)
+
+    # String Representation
+    def __str__(self):
+        return "reg@0x%(addr)02X: 0x%(val)02X" % \
+                { "addr" : self.addr , "val" : self.read() }
+
+    # Instance methods
+    # Set bits in register
+    def set_bits(self, bm):
+        self.contents = (self.contents | bm) & self.write_bitmask
+
+    # Clear bits in register
+    def clear_bits(self, bm):
+        self.contents = (self.contents & ~bm) & self.write_bitmask
+
+    # Nibble swap
+    # TODO?
+
+# Main function --------------------------------------------------------------
+## Main (for testing purposes)
+if __name__=="__main__":
+    print "Creating new register object"
+    reg = AVRRegister(0x00)
+    print reg
+
+    print "Writing 0x55 to register"
+    reg.write(0x55)
+    print reg
+
+    print "Writing 0xFE to register"
+    reg.write(0xFE)
+    print reg
+
+    print "Incrementing register (should be 0xFF, no overflow/carry)"
+    ovf = reg.incr()
+    print str(reg) + " overflow: " + str(ovf)
+
+    print "Incrementing register (should be 0x00 with overflow/carry)"
+    ovf = reg.incr()
+    print str(reg) + " overflow: " + str(ovf)
+
+    print "Decrementing register (should be 0xFF with overflow/carry)"
+    ovf = reg.decr()
+    print str(reg) + " overflow: " + str(ovf)
+
+    print "Incrementing register (should be 0xFE, no overflow/carry)"
+    ovf = reg.decr()
+    print str(reg) + " overflow: " + str(ovf)
+
+    print "Setting bit 0 (should be 0xFF)"
+    reg.set_bits(0x01)
+    print reg
+
+    print "Clearing bit 5 (should be 0xDF)"
+    reg.clear_bits(0x20)
+    print reg
+
